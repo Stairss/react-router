fffc0b635a7fe340ef709b151b5622f5a6f90f97
Dawid Spisak <dawidspisak@gmail.com>
2021-03-30 11:16:55 +0200

    Updates

---
+++ precache-manifest.1e0f0aa2ee9925ae797b8df08cca4b2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "51875d176cd6fc945535",
+    "url": "/router-app/./static/css/main.7673e6a8.chunk.css"
+  },
+  {
+    "revision": "51875d176cd6fc945535",
+    "url": "/router-app/./static/js/main.7cf6a603.chunk.js"
+  },
+  {
+    "revision": "55d9ca433ebad9247e95",
+    "url": "/router-app/./static/js/runtime~main.e309899f.js"
+  },
+  {
+    "revision": "87f145844538af824e9e",
+    "url": "/router-app/./static/js/2.81a91cc0.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/router-app/./static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/router-app/./static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/router-app/./static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "b4c0f662c8c84253349a20b33bf19a9f",
+    "url": "/router-app/./index.html"
+  }
+];
